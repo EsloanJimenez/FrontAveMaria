609076f201d627946bf1983648f2ed3775455a90
ejpservice <ejptheservice@gmail.com>
2023-06-02 14:01:41 -0400

    boton del back end poniendolo invisible

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -67,7 +67,7 @@ export const Header = () => {
                   </li>
                   <li><Link to="/views/leaders">Estadisticas</Link></li>
                   <li><Link to="/views/rules">Reglas</Link></li>
-                  <li><Link to="/be/views/admin">Back End</Link></li>
+                  <li className='invisible'><Link to="/be/views/admin">Back End</Link></li>
                 </ul>
             </nav>
          </header>
