c5620f20d97b708a3e5874df8f4f9b4a098b0ea7
ejpservice <ejptheservice@gmail.com>
2023-06-27 16:13:38 -0400

    disenando el live

--- src/views/LiveBroadcast.js
+++ src/views/LiveBroadcast.js
@@ -1,11 +1,23 @@
+import '../css/live.css'
+
 export const LiveBroadcast = () => {
    return(
-      <div className="rankings">
-         <iframe width="1048" height="590" src="https://www.youtube.com/embed/ZkLSS_qliUc" title="Transmision a sitio web" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+      <div className="live">
+         <iframe 
+            width="1048" 
+            height="590" 
+            src="https://www.youtube.com/embed/ZkLSS_qliUc" 
+            title="Transmision a sitio web" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            allowfullscreen
+         ></iframe>
 
-         </iframe>
-
-         <iframe width="360" height="540" src="https://www.youtube.com/live_chat?v=ZkLSS_qliUc&embed_domain=torneosalomeurena.netlify.app"></iframe>
+         <iframe 
+            width="65%" 
+            height="600" 
+            src="https://www.youtube.com/live_chat?v=ZkLSS_qliUc&embed_domain=torneosalomeurena.netlify.app"
+         ></iframe>
       </div>
    )
 }
