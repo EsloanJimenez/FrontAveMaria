42c4c99dbe314bf50374fe1d6593abdd78a5304f
ejpservice <ejptheservice@gmail.com>
2023-06-27 15:29:17 -0400

    piniendole chat al en vivo

--- src/views/LiveBroadcast.js
+++ src/views/LiveBroadcast.js
@@ -4,6 +4,8 @@ export const LiveBroadcast = () => {
          <iframe width="1048" height="590" src="https://www.youtube.com/embed/ZkLSS_qliUc" title="Transmision a sitio web" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
 
          </iframe>
+
+         <iframe src="https://www.youtube.com/live_chat?v=ZkLSS_qliUc&embed_domain=torneosalomeurena.netlify.app"></iframe>
       </div>
    )
 }
