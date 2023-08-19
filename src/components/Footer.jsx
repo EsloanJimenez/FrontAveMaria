c462c317d55d6e767901138d5d9a4083d1977ae5
ejpservice <ejptheservice@gmail.com>
2023-08-19 11:37:39 -0400

    intagran de las estadisticas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,7 @@ export const Footer = ({visitCounter}) => {
          <ul>
             <li><FontAwesomeIcon icon={faInstagram} /><a target="_blank" href="https://www.instagram.com/motivofotodigital/">Motivo Foto Digital</a></li>
             <li><FontAwesomeIcon icon={faInstagram} /><a target="_blank" href="https://www.instagram.com/estrellas_del_salome_urena/">Estrellas Del Salome Ureña</a></li>
+            <li><FontAwesomeIcon icon={faInstagram} /><a target="_blank" href="https://www.instagram.com/estadisticas_torneo_baloncesto/">Estadisticas Torneo De Baloncesto</a></li>
             <li><FontAwesomeIcon icon={faEye} /><span>{visitCounter+1}</span></li>
          </ul>
       </div>
